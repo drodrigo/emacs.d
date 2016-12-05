985d69a385d423534574ad9f026ce564696c3b65
dyego <dyego9444@hotmail.com>
2016-12-05 00:11:37 -0200

    new configuration and packages

--- init.el
+++ init.el
@@ -64,13 +64,19 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
+;;Elpy
+(elpy-enable)
 
+;;Jedi
+(add-hook 'python-mode-hook 'jedi:setup)
 
-
+;;Ansi-term
+(add-hook 'term-mode-hook (lambda()
+        (setq yas-dont-activate t)))
 
 ;;Personal config after
 ;;(load-theme 'solarized- t)
-(load-theme 'gruvbox t)
+(load-theme 'solarized-light t)
 (global-linum-mode t)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
@@ -85,13 +91,19 @@
 
 ;;JS2 Mode
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
+
+;;WeB Mode
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 
 
 ;;Yaml mode
-(add-to-list 'auto-mode-alist '("\\.js\\'" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
 
 ;;Less mode
-(add-to-list 'auto-mode-alist '("\\.js\\'" . less-css-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . less-css-mode))
+(add-to-list 'auto-mode-alist '("\\.less\\'" . less-css-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . less-css-mode))
 
 
 
@@ -159,13 +171,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("427fed191e7a766152e59ef0e2904283f436dbbe259b9ccc04989f3acde50a55" "8abee8a14e028101f90a2d314f1b03bed1cde7fd3f1eb945ada6ffc15b1d7d65" "a164837cd2821475e1099911f356ed0d7bd730f13fa36907895f96a719e5ac3e" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+    ("b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "427fed191e7a766152e59ef0e2904283f436dbbe259b9ccc04989f3acde50a55" "8abee8a14e028101f90a2d314f1b03bed1cde7fd3f1eb945ada6ffc15b1d7d65" "a164837cd2821475e1099911f356ed0d7bd730f13fa36907895f96a719e5ac3e" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(menu-bar-mode nil)
  '(tool-bar-mode nil))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "MS  " :slant normal :weight bold :height 128 :width normal)))))
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
