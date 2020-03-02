f3b4b03d0151be0e462d1a34d0893ec107a52ed1
Dyego Lima <dyego9444@gmail.com>
2020-03-01 22:31:52 -0300

    dooms.d config

--- elpa/realgud-20160829.1821/realgud-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; realgud-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "realgud" "realgud.el" (22595 34123 123001
-;;;;;;  421000))
-;;; Generated autoloads from realgud.el
-
-(defconst realgud--recursive-autoloads-file-name "realgud-recursive-autoloads.el" "\
-Where to store autoloads for subdirectory contents.")
-
-(defconst realgud--recursive-autoloads-base-directory (file-name-directory (if load-in-progress load-file-name buffer-file-name)))
-
-(with-demoted-errors "Error in RealGUD's autoloads: %s" (load (expand-file-name realgud--recursive-autoloads-file-name realgud--recursive-autoloads-base-directory) t t))
-
-;;;***
-
-;;;### (autoloads nil nil ("realgud-pkg.el") (22595 34127 684997
-;;;;;;  195000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; realgud-autoloads.el ends here
